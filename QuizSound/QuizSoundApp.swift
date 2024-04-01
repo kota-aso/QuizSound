b7fb31e3b80197e4ccbd81e5e351040c7bd75889
kota-aso <kotaaso1212@gmail.com>
2024-04-01 22:16:37 +0900

    Initial Commit

---
+++ QuizSound/QuizSoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizSoundApp.swift
+//  QuizSound
+//
+//  Created by Kota Aso on 2024/04/01.
+//
+
+import SwiftUI
+
+@main
+struct QuizSoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
