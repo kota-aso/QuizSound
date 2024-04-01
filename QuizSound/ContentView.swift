609e5b442cea89b6ea33f638ab02d5a317eae6e1
kota-aso <kotaaso1212@gmail.com>
2024-04-01 22:48:52 +0900

    firstCommit

--- QuizSound/ContentView.swift
+++ QuizSound/ContentView.swift
@@ -10,10 +10,54 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Spacer()
+            Button {
+            } label: {
+                Text("クイズ出題")
+                    .font(.largeTitle)
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(.red)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            Spacer()
+            Button {
+            } label: {
+                Text("シンキングタイム\n(10秒)")
+                .font(.largeTitle)
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(.green)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                .frame(maxWidth: .infinity)
+            }
+            Spacer()
+            HStack {
+                Button {
+                } label: {
+                Image(systemName: "circle")
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(.red)
+                    .cornerRadius(10)
+                }
+                Button {
+                } label: {
+                Image(systemName: "xmark")
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(.blue)
+                    .cornerRadius(10)
+                }
+            }
+            Spacer()
+            
         }
         .padding()
     }
